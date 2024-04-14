19835e281dd0d24d00d099835e3f37d1342718a0
Lily Tsai <ltsai36@gatech.edu>
2024-04-14 00:55:11 -0500

    make images align

--- app.py
+++ app.py
@@ -27,18 +27,18 @@ st.write(
 col1, col2 = st.columns(2)
 with col1:
     uploaded_file1 = st.file_uploader("Upload first image...", type=['jpg', 'jpeg', 'png'])
+    if uploaded_file1 is not None:
+        image1 = Image.open(uploaded_file1)
+        st.image(image1, caption='Image 1', width=300)
 
 # Upload second image
 with col2:
     uploaded_file2 = st.file_uploader("Upload second image...", type=['jpg', 'jpeg', 'png'])
+    if uploaded_file2 is not None:
+        image2 = Image.open(uploaded_file2)
+        st.image(image2, caption='Image 2', width=300)
 
 if uploaded_file1 and uploaded_file2:
-    # Read and display the uploaded images
-    image1 = Image.open(uploaded_file1)
-    image2 = Image.open(uploaded_file2)
-
-    st.image([image1, image2], caption=['Image 1', 'Image 2'], width=300)
-
     # Resize images to the same size
     min_width = min(image1.width, image2.width)
     min_height = min(image1.height, image2.height)
@@ -57,7 +57,7 @@ if uploaded_file1 and uploaded_file2:
             win_size -= 1
 
         # Calculate Structural Similarity Index (SSI) with the valid window size
-        similarity_index = ssim(image1_gray, image2_gray, win_size=win_size)
+        similarity_index = ssim(image1_gray, image2_gray, win_size=win_size, data_range=1)
 
         st.write(f'Similarity Index: {similarity_index}')
 
